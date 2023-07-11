f290b7c04f69d227ea291570b93af1e6d31f1437
IvanAzcurra <chepes_ivan@hotmail.com>
2023-07-11 01:43:11 -0300

    Ejercicio 1.12 completado

---
+++ Parte 1/anecdotes/src/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+const Button = ({ changeAnecdote, text }) => (
+  <button onClick={changeAnecdote}>{text}</button>
+);
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  let index = selected;
+  const randomIndex = () => Math.floor(Math.random() * props.anecdotes.length);
+
+  const changeAnecdote = () => {
+    setSelected(index = randomIndex());
+    console.log(index);
+  };
+
+  return (
+    <div>
+      {props.anecdotes[index]}
+      <br />
+      <Button changeAnecdote={changeAnecdote} text="next anecdote" />
+    </div>
+  );
+};
+
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App anecdotes={anecdotes} />);
