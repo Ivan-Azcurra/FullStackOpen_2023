7dede2c307041a694bf7965a740d1d67ebcc3409
IvanAzcurra <chepes_ivan@hotmail.com>
2023-07-12 02:11:48 -0300

    Ejercicio 1.14 completado

--- Parte 1/anecdotes/src/index.js
+++ Parte 1/anecdotes/src/index.js
@@ -1,36 +1,57 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>{text}</button>
-);
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const MostVoted = ({ anecdotes, points }) => {
+  const maxVoted = Math.max(...points);
+  const positionWin = points.indexOf(maxVoted);
+  const anecdoteWin = anecdotes[positionWin];
+
+  if (maxVoted === 0) {
+    return (
+      <div>
+        <p>No vote</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p>{anecdoteWin}</p>
+      <p>has {maxVoted} votes</p>
+    </div>
+  );
+};
 
 const App = (props) => {
   const [selected, setSelected] = useState(0);
-  const [ponits, setPoints] = useState(Array(props.anecdotes.length).fill(0));
+  //State de votos
+  const [points, setPoints] = useState(Array(props.anecdotes.length).fill(0));
 
   const changeAnecdote = () => {
     const index = Math.floor(Math.random() * props.anecdotes.length);
     setSelected(index);
-    console.log(index);
   };
 
   const increaseVote = () => {
-    const newPoints = [...ponits];
+    const newPoints = [...points];
     newPoints[selected] += 1;
     //le pasamos el nuevo array copiado de ...points, modificado
     setPoints(newPoints);
-    console.log(ponits[selected]);
   };
 
   return (
     <div>
-      {props.anecdotes[selected]}
-      <br />
-      has {ponits[selected]} votes
-      <br />
+      <Header text={"Anecdote of the day"} />
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
       <Button onClick={increaseVote} text={"vote"} />
       <Button onClick={changeAnecdote} text="next anecdote" />
+      <Header text={"Anecdote with most votes"} />
+      <MostVoted anecdotes={props.anecdotes} points={points} />
     </div>
   );
 };
